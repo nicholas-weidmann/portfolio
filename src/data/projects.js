3ca2e1b718cc835ce3a54aad7c43d45498569a70
Nicholas Weidmann <nicholas.weidmann@hotmail.com>
2023-11-25 03:19:18 +0100

    fix category_tags spacing & add project room_manager

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ export const projects = [
       name: 'Test Client',
       description: 'A test client for testing the API',
       text: 'This is a test client for testing the API',
-      category_tags: ['Game Dev'],
+      category_tags: ['Game Dev', 'Web Design'],
       technology_tags: ['Angular', 'API', 'OpenID Connect'],
     },
   },
@@ -21,6 +21,18 @@ export const projects = [
       text: 'This is my portfolio website',
       category_tags: ['Web Design'],
       technology_tags: ['Astro', 'Tailwind CSS', 'View Transition', 'Open Graph'],
+    },
+  },
+  {
+    params: {
+      project: 'room_manager',
+    },
+    props: {
+      name: 'Room Manager',
+      description: 'API for game backend',
+      text: 'This is the API for the game backend',
+      category_tags: ['Game Dev', 'Infra'],
+      technology_tags: ['NestJS', 'Lambda', 'DynamoDB', 'REST', 'OpenAPI'],
     },
   },
 ]
