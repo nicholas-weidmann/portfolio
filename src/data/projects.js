c5deee1dab5e3d8529ac904b6d59e32b03d1f060
Nicholas Weidmann <nicholas.weidmann@hotmail.com>
2023-11-25 02:38:34 +0100

    parametrize dynamic project page

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,1 +1,14 @@
-export const projects = [{ params: { project: 'test_client' } }]
+export const projects = [
+  {
+    params: {
+      project: 'test_client',
+    },
+    props: {
+      name: 'Test Client',
+      description: 'A test client for testing the API',
+      text: 'This is a test client for testing the API',
+      category_tags: ['Game Dev'],
+      technology_tags: ['Angular', 'API', 'OpenID Connect'],
+    },
+  },
+]
